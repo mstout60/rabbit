1a471dd8489dc7bbc80c4c8efdc57b98e44cc018
mstout60 <mstout@eprospector.com>
2025-02-04 20:11:32 -0600

    Implement Cart, Navbar, Footer, User Layout

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { IoMdClose } from "react-icons/io";
+import CartContents from "../Cart/CartContents";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
         drawerOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
@@ -13,6 +14,21 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
         <button onClick={toggleCartDrawer}>
           <IoMdClose className="size-6 text-gray-600" />
         </button>
+      </div>
+      {/* Card contents with scrollable area */}
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        {/* Component for the Cart contents */}
+        <CartContents />
+      </div>
+      {/* Checkout button fixed at the bottom */}
+      <div className="p-4 bg-white sticky bottom-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+          Checkout
+        </button>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+          Shipping,taxes and discount codes calculated at checkout.
+        </p>
       </div>
     </div>
   );
